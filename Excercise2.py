0351c117cfa5f3121dc7a975cead94174c9908a5
nicho <nicholas.thaut@stud.tu-darmstadt.de>
2022-11-18 13:17:59 +0100

    played around with scientific mode: no significant changes

--- Excercise2.py
+++ Excercise2.py
@@ -12,13 +12,28 @@ data = pd.read_csv(url, sep=" ", header=None,
                           "P30", "Nf", "Nc", "epr", "Ps30", "phi", "NRf", "NRc", "BPR", "farB", "htBleed", "Nf_dmd",
                           "PCNfR_dmd", "W31", "W32", "NaN", "NaN2"])
 
-data = data.drop(columns=["NaN", "NaN2"])
+df = data.drop(columns=["NaN", "NaN2"])
+
+# %%
+fig = plt.figure(figsize=[15,20])
+sensor_number = 1
+for param in df.columns[5:27]:
+    sbp = plt.subplot(7,3,sensor_number)
+    df[param].hist()
+    sbp.set_title(param)
+    plt.tight_layout()
+    sensor_number+=1
+
+fig.show()
+# %%
+description = df.describe()
+# Columns 1:5 are not sensor data
+# sensors T2, P2, P15, Nf, epr, NRf, BPR, farB, Nf_dmd, PCNfR_dmd are constant
+# %%
 
 
-plt.figure()
-data[['Altitude']].boxplot()
-plt.figure()
-data[["MachNumber"]].boxplot()
-
+# %%
+sensor_data = df.iloc[:, 5:26]
+sensor_data = sensor_data.drop(columns= ['T2', 'P2', 'P15', 'epr', 'farB', 'Nf_dmd', 'PCNfR_dmd'])
 
 # %%
